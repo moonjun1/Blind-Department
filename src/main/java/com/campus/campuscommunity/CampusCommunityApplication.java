be967257a2f9e9ceea97b48b7987a7ec7345e134
moonjun1 <puls55555@naver.com>
2025-04-24 14:44:02 +0900

    init : 초기설정

---
+++ src/main/java/com/campus/campuscommunity/CampusCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.campus.campuscommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campus/campuscommunity/CampusCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.campuscommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
